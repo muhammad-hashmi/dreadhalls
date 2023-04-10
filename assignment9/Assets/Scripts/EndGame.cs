3c5b7c1014892e1c99e4a3e3f5dfd729646665d1
muhammad-hashmi <78245613+muhammad-hashmi@users.noreply.github.com>
2023-04-10 00:07:03 -0400

    Assignment 9
    Assignment 9

---
+++ assignment9/Assets/Scripts/EndGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EndGame: MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.y <= -1) {
+            GameObject.Destroy(GameObject.Find("WhisperSource"));
+            SceneManager.LoadScene("GameOver");       
+        }
+    }
+}
---
+++ assignment9/Assets/Scripts/LevelDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelDisplay : MonoBehaviour {
+
+    public Text Level;
+
+    public static int levelNumber = 1;
+
+	// Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Level.text = "Level " + levelNumber;
+    }
+}
